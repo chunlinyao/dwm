c6a7f4b48ee3b354249c46149e727513c8762228
Yao Chunlin <chunlinyao@gmail.com>
2013-07-19 16:20:05 +0800

    my config update

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const Bool topbar            = True;     /* False means bottom bar */
 static const Bool focusonwheelscroll = False;
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { ":)", "@", "W", "++", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
